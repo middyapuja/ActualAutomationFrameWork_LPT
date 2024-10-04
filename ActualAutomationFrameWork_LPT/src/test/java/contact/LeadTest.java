1229ad017239a83794029d2c1d8c59dc51fbac9f
PUJA MIDDYA <PUJA MIDDYA@DESKTOP-7Q6STK1>
2024-10-04 20:03:51 +0530

    Pushing test Scripts

---
+++ ActualAutomationFrameWork_LPT/src/test/java/contact/LeadTest.java
@@ -0,0 +1,11 @@
+package contact;
+
+
+
+public class LeadTest {
+	public static void main(String[] args) {
+		System.out.println("fork operation ");
+	}
+	
+
+}
---
+++ ActualAutomationFrameWork_LPT/src/test/java/organization/CreateContactTest.java
@@ -0,0 +1,8 @@
+package organization;
+
+public class CreateContactTest {
+public static void main(String[] args) {
+	
+	System.out.println("Crate Contact");
+}
+}
